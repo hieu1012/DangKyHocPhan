36819416c9249edb977d1fc8de8686d36807efb9
bichdao090202 <bichdao090202@gmail.com>
2024-06-06 14:22:31 +0700

    get api login

--- src/users/ListUser.js
+++ src/users/ListUser.js
@@ -1,34 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-export default class ListUser extends React.Component {
-  state = {
-    users: [],
-  };
+export default function ListUser() {
+  const [users, setUsers] = useState([]);
+   
 
-  async componentDidMount() {
-    let res = await axios.get("https://reqres.in/api/users?page=2");
-    this.setState({ users: res && res.data && res.data.data ? res.data.data : []
+  useEffect(() => {
+    axios.get("https://reqres.in/api/users?page=2")
+      .then(response => {
+        setUsers(response.data.data);
+        console.log(users)
+      })
+      .catch(error => {
+        console.error(`Error fetching data: ${error}`);
+      });
+    const userData = {
+      tenDangNhap: "10001",
+      matKhau: "1111"
+    };
+    axios.post("http://localhost:8080/SinhVien/login", userData)
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error(`Error fetching data: ${error}`);
+      });
+  }, []);
 
-    });
-  }
 
-  render() {
-    let { users } = this.state;
-    return (
+  return (
+    <div>
+      <h1>List of Users</h1>
       <div>
-        <h1>List of Users</h1>
-        <div>
-            {users && users.length >0 && users.map((user, index) => {
-                return(
-                    <div key={index}>
-                        {index +1} - {user.first_name} {user.last_name}
-                    </div>
-                )   
-            })
-            }
-        </div>
+        {users && users.length > 0 && users.map((user, index) => {
+          return (
+            <div key={index}>
+              {index + 1} - {user.first_name} {user.last_name}
+            </div>
+          )
+        })
+        }
       </div>
-    );
-  }
+    </div>
+  );
+
 }
